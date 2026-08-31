c5ddc6dfcf5105751dea859d9f908813c0ebfe34
Montukharb <montukharb868@gmail.com>
2026-08-31 20:09:56 +0530

    DbConnection & Migration Custom Command Setup

---
+++ Application/FileProcessing.WebSolution.ModuleComposition/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileProcessing.WebSolution.ModuleComposition
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddModuleComposition(this IServiceCollection services)
+        {
+            
+            return services;
+        }
+    }
+}
---
+++ Application/UserManagement.PL/SeedData/UserManagementSeedData.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserManagement.PL.SeedData
+{
+    internal class UserManagementSeedData
+    {
+    }
+}
