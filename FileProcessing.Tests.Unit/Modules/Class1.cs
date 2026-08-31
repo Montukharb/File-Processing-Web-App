e59c57aab7b691ca696baab57278a8697d5e9799
Montukharb <montukharb868@gmail.com>
2026-08-31 14:35:05 +0530

    fix dir and solution folder issue

---
+++ FileProcessing.Tests.Unit/Modules/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileProcessing.Tests.Unit.Modules
+{
+    internal class Class1
+    {
+    }
+}
---
+++ FileProcessing.Tests.Unit/infrastructure/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileProcessing.Tests.Unit.infrastructure
+{
+    internal class Class1
+    {
+    }
+}
